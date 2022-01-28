6869209c199e42117ac89d444ff08e91632e0cec
whatkimkong <whatkimkong@gmail.com>
2022-01-28 10:16:43 +0100

    mod: remove home list links

---
+++ old.js
@@ -0,0 +1,25 @@
+
+/* OLD CODE 
+
+<Main>
+       <List spacing={3} my={0}>
+        <ListItem>
+          <ListIcon as={PlusSquareIcon} color="green.600" />
+          <ChakraLink
+            isExternal
+            href="https://www.linkedin.com/in/kim-lyons-hernandez/"
+            flexGrow={1}
+            mr={2}
+          >
+            Go to your Kanban <ArrowForwardIcon />
+          </ChakraLink>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={PlusSquareIcon} color="green.600" />
+          <ChakraLink isExternal href="https://github.com/whatkimkong" flexGrow={1} mr={2}>
+            Find out more <QuestionOutlineIcon />
+          </ChakraLink>
+        </ListItem>
+      </List>
+    </Main>
+ */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,27 +19,7 @@ import { Footer } from '../components/Footer'
 const Index = () => (
   <Container height="100vh">
     <Hero />
-    <Main>
-       <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={PlusSquareIcon} color="green.600" />
-          <ChakraLink
-            isExternal
-            href="https://www.linkedin.com/in/kim-lyons-hernandez/"
-            flexGrow={1}
-            mr={2}
-          >
-            Go to your Kanban <ArrowForwardIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={PlusSquareIcon} color="green.600" />
-          <ChakraLink isExternal href="https://github.com/whatkimkong" flexGrow={1} mr={2}>
-            Find out more <QuestionOutlineIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+    
     <DarkModeSwitch />
     <Footer>
       <Text>
