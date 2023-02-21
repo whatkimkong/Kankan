9c80eddc41d5d4d5953db5c93beaec469c399154
whatkimkong <whatkimkong@gmail.com>
2023-02-21 22:21:46 +0000

    add: signup page

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,25 @@
+import {
+    Text,
+    Code,
+} from '@chakra-ui/react'
+import { Headline } from '../../components/Headline'
+import { Container } from '../../components/Container'
+import { DarkModeSwitch } from '../../components/DarkModeSwitch'
+import { CTA } from '../../components/CTA'
+import { Footer } from '../../components/Footer'
+
+const Signup = () => (
+<Container height="100vh">
+    <Headline title="Sign Up" />
+    <DarkModeSwitch />
+    <Footer>
+    <Text>
+        Built using <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
+        <Code>TypeScript</Code>.
+    </Text>
+    </Footer>
+    <CTA />
+</Container>
+)
+
+export default Signup;
