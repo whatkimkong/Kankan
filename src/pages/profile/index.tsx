1eb02f2a8f30ec7beabf6202df37236fd054b1d0
whatkimkong <whatkimkong@gmail.com>
2023-02-23 22:34:43 +0000

    add: profile api route

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,26 @@
+import { getSession } from 'next-auth/react';
+
+import UserProfile from '../../components/profile/userProfile'
+
+function ProfilePage() {
+  return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
+
+export default ProfilePage;
